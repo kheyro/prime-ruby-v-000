9c856d40ad49a4a0190ab5453d93122777ce5b6f
Denis Atkesone <denis.atkesone@gmail.com>
2017-12-30 00:19:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 def prime?(number)
-
+  i = 2
   # find nearest sqrt
   nearest_root = Math.sqrt(number).round
   # modulo to all number until sqrt, if no divisible found, then it is a prime
+  while i < nearest_root
+    return true if ( nearest_root % i == 0)
+  end
 
+  false
 end
